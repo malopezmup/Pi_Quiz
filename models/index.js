b39af1951bcf16d1db07535b5a7a72b52a3ef7d7
malopezmup <36400943+malopezmup@users.noreply.github.com>
2018-06-09 12:51:23 +0200

    v1

--- models/index.js
+++ models/index.js
@@ -16,26 +16,25 @@ const sequelize = new Sequelize(url);
 // Import the definition of the Quiz Table from quiz.js
 sequelize.import(path.join(__dirname, 'quiz'));
 
-// Import the definition of the Tips Table from tip.js
-sequelize.import(path.join(__dirname,'tip'));
-
-// Import the definition of the Users Table from user.js
-sequelize.import(path.join(__dirname,'user'));
-
 // Session
 sequelize.import(path.join(__dirname,'session'));
 
-
-// Relation between models
-
-const {quiz, tip, user} = sequelize.models;
-
-tip.belongsTo(quiz);
-quiz.hasMany(tip);
-
-// Relation 1-to-N between User and Quiz:
-user.hasMany(quiz, {foreignKey: 'authorId'});
-quiz.belongsTo(user, {as: 'author', foreignKey: 'authorId'});
+// Create tables
+sequelize.sync()
+    .then(() => sequelize.models.quiz.count())
+    .then(count => {
+        if (!count){
+            return sequelize.models.quiz.bulkCreate([
+                {question: "Capital de Italia", answer: "Roma"},
+                {question: "Capital de Francia", answer: "París"},
+                {question: "Capital de España", answer: "Madrid"},
+                {question: "Capital de Portugal", answer: "Lisboa"}
+            ]);
+        }
+    })
+    .catch(error => {
+        console.log(error);
+    });
 
 
 module.exports = sequelize;
